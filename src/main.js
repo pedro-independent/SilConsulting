658736bcf8c46479189045587cb121393d950515
Nevvees <pedro@independent.pt>
2025-06-25 14:56:41 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -50,37 +50,37 @@ barba.init({
 
 
 /* Menu Open  */
-// const dropdown = document.querySelector('.dropdown-nav-link');
-// const navbar = document.querySelector('.navbar');
-// const navServicesWrapper = document.querySelector('.nav-services-list-wrap');
+const dropdown = document.querySelector('.dropdown-nav-link');
+const navbar = document.querySelector('.navbar');
+const navServicesWrapper = document.querySelector('.nav-services-list-wrap');
 
-// function expandNavbar() {
-//     gsap.to(navbar, {
-//         height: "13em",
-//         duration: 0.4,
-//         ease: "power2.out",
-//         onComplete: () => {
-//             navServicesWrapper.style.display = "flex";
-//             gsap.fromTo(navServicesWrapper, 
-//                 { opacity: 0 }, 
-//                 { opacity: 1, duration: 0.3, ease: "power2.out" }
-//             );
-//         }
-//     });
-// }
+function expandNavbar() {
+    gsap.to(navbar, {
+        height: "13em",
+        duration: 0.4,
+        ease: "power2.out",
+        onComplete: () => {
+            navServicesWrapper.style.display = "flex";
+            gsap.fromTo(navServicesWrapper, 
+                { opacity: 0 }, 
+                { opacity: 1, duration: 0.3, ease: "power2.out" }
+            );
+        }
+    });
+}
 
-// function collapseNavbar() {
-//     gsap.to(navServicesWrapper, {
-//         opacity: 0,
-//         duration: 0.3,
-//         ease: "power2.in",
-//         onComplete: () => {
-//             navServicesWrapper.style.display = "none";
-//             gsap.to(navbar, { height: "3em", duration: 0.4, ease: "power2.in" });
-//         }
-//     });
-// }
+function collapseNavbar() {
+    gsap.to(navServicesWrapper, {
+        opacity: 0,
+        duration: 0.3,
+        ease: "power2.in",
+        onComplete: () => {
+            navServicesWrapper.style.display = "none";
+            gsap.to(navbar, { height: "3em", duration: 0.4, ease: "power2.in" });
+        }
+    });
+}
 
-// // Event Listeners
-// dropdown.addEventListener("mouseenter", expandNavbar);
-// navbar.addEventListener("mouseleave", collapseNavbar);
+// Event Listeners
+dropdown.addEventListener("mouseenter", expandNavbar);
+navbar.addEventListener("mouseleave", collapseNavbar);
