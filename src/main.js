fb53607e6bfe00df29c22570218f62a6e07003ea
pedro-independent <pedro@independent.pt>
2025-06-30 21:57:27 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,53 +1,6 @@
 import './styles/style.css'
-import barba from '@barba/core'
-
-
-console.log(barba);
 
 gsap.registerPlugin();
-
-/* Barba.JS */
-const overlay = document.querySelector('.transition-overlay');
-
-const animationLeave = (container) => {
-  return gsap.timeline()
-    .to(overlay, {
-      opacity: 0.5,
-      duration: 0.5,
-      ease: 'power2.out'
-    });
-};
-
-const animationEnter = (container) => {
-  return gsap.timeline()
-    .set(container, {
-      y: '100vh',
-      opacity: 1
-    }) 
-    .to(container, {
-      y: '0vh',
-      duration: 1,
-      ease: 'power4.out'
-    })
-    .to(overlay, {
-      opacity: 0,
-      duration: 0.5,
-      ease: 'power2.in'
-    }, '-=0.5'); // overlap fade-out
-};
-
-barba.init({
-  transitions: [{
-    async leave({ current }) {
-      await animationLeave(current.container);
-    },
-    async enter({ next }) {
-      await animationEnter(next.container);
-    }
-  }]
-});
-
-
 
 /* Menu Open  */
 const dropdown = document.querySelector('.dropdown-nav-link');
